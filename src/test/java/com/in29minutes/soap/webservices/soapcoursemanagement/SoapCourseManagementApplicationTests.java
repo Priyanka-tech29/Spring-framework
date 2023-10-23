dc0bf3664ebdb8e2a6bf52c41b9a5c82b26bfee8
Priyanka-tech29 <paridhiscriet@gmail.com>
2023-10-23 09:44:27 +0530

    SOAP

---
+++ src/test/java/com/in29minutes/soap/webservices/soapcoursemanagement/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in29minutes.soap.webservices.soapcoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
